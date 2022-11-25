f93970775ea8ca5d393eaf5a08adceeaa1632d8f
Evg-tr-r <116937894+Evg-tr-r@users.noreply.github.com>
2022-11-25 20:20:05 +0300

    1-3

--- style.js
+++ style.js
@@ -1,3 +1,12 @@
 function func1(a){
     console.log(a*a);
 }func1(3);
+function func2(a,b){
+    const result=a+b;
+    console.log(result);
+}func2(2,3);
+function func3(a,b,c){
+    const result=(a-b)/c;
+    console.log(result);
+}func3(10,2,2);
+
