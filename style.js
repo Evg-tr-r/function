60d8848c3bdc5e22b53096c472c06685e00212fd
Evg-tr-r <116937894+Evg-tr-r@users.noreply.github.com>
2022-11-25 22:32:54 +0300

    5

--- style.js
+++ style.js
@@ -10,3 +10,22 @@ function func3(a,b,c){
     console.log(result);
 }func3(10,2,2);
 
+
+
+
+/*function func4(day){
+    const days=[1=>'sunday','monday'];
+    console.log(day);
+
+}func4();*/
+
+
+
+
+
+function func5(a,b){
+    if (a===b) {
+    return true;
+} 
+   return false;
+}func5(2,3);
